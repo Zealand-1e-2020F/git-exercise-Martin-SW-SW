3fb4385dfaf22ad6f20e68b232a8561cbf058d17
Martin-SW-SW <62882502+Martin-SW-SW@users.noreply.github.com>
2020-04-01 13:01:23 +0200

    Update myClass copy.cs

--- myClass copy.cs
+++ myClass copy.cs
@@ -5,11 +5,13 @@ namespace GitExercise
 public string Adresse { get; set; }
 public string Navn { get; set; }
 public int Postnr { get; set; }
-public myClass2(string navn, string adresse, int postnr)
+public string By { get; set; }
+public myClass2(string navn, string adresse, int postnr, string by)
 {
     this.Navn = navn;
     this.Adresse = adresse;
     this.Postnr = postnr;
+    this.By = by;
 }
 public override string ToString(){
     return $"{Navn} Lives at {Adresse}, {Postnr}";
